9d16cfd601bf30eba53d41f1a14ea6de6643eb11
Simon Willison <swillison@gmail.com>
2019-06-24 17:58:00 -0700

    Make some columns searchable

--- fetch_data.py
+++ fetch_data.py
@@ -5,13 +5,32 @@ import csv
 import sqlite_utils
 
 
+fts_columns = {
+    "FARA_All_Registrants": ["Name", "Address_1", "Address_2"],
+    "FARA_All_ForeignPrincipals": ["Foreign_Principal", "Registrant_Name"],
+    "FARA_All_RegistrantDocs": [
+        "Registrant_Name",
+        "Short_Form_Name",
+        "Foreign_Principal_Name",
+    ],
+    "FARA_All_ShortForms": [
+        "Short_Form_Last_Name",
+        "Short_Form_First_Name",
+        "Registrant_Name",
+    ],
+}
+
+
 def make_doc(data):
+    new_data = {}
     for key, value in data.items():
-        if key.endswith(" Date") and "/" in value:
+        if "Date" in key and "/" in value:
             # Convert 06/11/2019 to yyyy-mm-dd
             mm, dd, yyyy = value.split("/")
-            data[key] = "{}-{}-{}".format(yyyy, mm, dd)
-    return data
+            value = "{}-{}-{}".format(yyyy, mm, dd)
+        key = key.replace(" ", "_")
+        new_data[key] = value
+    return new_data
 
 
 if __name__ == "__main__":
@@ -29,4 +48,8 @@ if __name__ == "__main__":
         reader = csv.reader(StringIO(zipfile.open(filename).read().decode("utf-8")))
         headers = next(reader)
         docs = (make_doc(dict(zip(headers, row))) for row in reader)
-        db[filename.replace(".csv", "")].insert_all(docs)
+        table = filename.replace(".csv", "")
+        db[table].insert_all(docs)
+        # Set up FTS
+        if table in fts_columns:
+            db[table].enable_fts(fts_columns[table])
